b31183cb69444c221f4dec6a2dcd51acac20b9a0
Michal Bonowicz <michus1332@gmail.com>
2021-11-26 22:23:18 +0100

    add menu section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Menu from './components/menu'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Menu />
     </div>
   );
 }
---
+++ src/components/menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../styles/menu.css'
+
+export default function menu() {
+    return (
+        <div className="container">
+            <div className="logo">
+                <p className="logo-text">M&Music</p>
+            </div>
+        </div>
+    )
+}
