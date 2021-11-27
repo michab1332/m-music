ca380fa87361cb0f4a8bfc073b0a416276ecfa34
Michal Bonowicz <michus1332@gmail.com>
2021-11-27 15:08:28 +0100

    add css to menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import '../styles/menu.css'
 
-export default function menu() {
+function Menu() {
+    const [search, setSearch] = useState();
     return (
         <div className="container">
-            <div className="logo">
-                <p className="logo-text">M&Music</p>
+            <div className="container__wrapper">
+                <div className="container__logo">
+                    <p className="container__logo-text">M&Music</p>
+                </div>
+
+                <div className="container__rightWrapper">
+                    <form className="container__form">
+                        <input onChange={(e) => setSearch(e.target.value)} value={search} className="container__form-input" type="text" placeholder="search for some music" />
+                        <img className="container__form-searchSymbol" src={require('../images/searchSymbol.png').default} alt="search icon" />
+                    </form>
+
+                    <div className="container__logIn">
+                        <p className="container__logIn-text">Log in</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
+
+export default Menu
