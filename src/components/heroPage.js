d9edeae957021e00133fea1fd3a9f5dc2b30356a
Michal Bonowicz <michus1332@gmail.com>
2021-11-29 19:00:23 +0100

    add styles to heroPage

--- src/components/heroPage.js
+++ src/components/heroPage.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import '../styles/heroPage.css'
+
 function HeroPage() {
     return (
-        <div>
-            HeroPage
+        <div className="containerHero">
+            <section className="containerHero__wrapper">
+                <div className="containerHero__heroImage">
+                    <img className="containerHero__heroImage-vector1" src={require('../svg/vector1.svg').default} alt="vector1" />
+                    <img className="containerHero__heroImage-img" src={require('../images/pexels-anna-shvets-4315839.png').default} alt="anna" />
+                    <img className="containerHero__heroImage-vector2" src={require('../svg/vector2.svg').default} alt="vector2" />
+                </div>
+            </section>
         </div>
     )
 }
--- src/components/menu.js
+++ src/components/menu.js
@@ -5,20 +5,20 @@ import '../styles/menu.css'
 function Menu() {
     const [search, setSearch] = useState();
     return (
-        <div className="container">
-            <div className="container__wrapper">
-                <div className="container__logo">
-                    <p className="container__logo-text">M&Music</p>
+        <div className="containerMenu">
+            <div className="containerMenu__wrapper">
+                <div className="containerMenu__logo">
+                    <p className="containerMenu__logo-text">M&Music</p>
                 </div>
 
-                <div className="container__rightWrapper">
-                    <form className="container__form">
-                        <input onChange={(e) => setSearch(e.target.value)} value={search} className="container__form-input" type="text" placeholder="search music..." />
-                        <img className="container__form-searchSymbol" src={require('../images/searchSymbol.png').default} alt="search icon" />
+                <div className="containerMenu__rightWrapper">
+                    <form className="containerMenu__form">
+                        <input onChange={(e) => setSearch(e.target.value)} value={search} className="containerMenu__form-input" type="text" placeholder="search music..." />
+                        <img className="containerMenu__form-searchSymbol" src={require('../images/searchSymbol.png').default} alt="search icon" />
                     </form>
 
-                    <div className="container__logIn">
-                        <p className="container__logIn-text">Log in</p>
+                    <div className="containerMenu__logIn">
+                        <p className="containerMenu__logIn-text">Log in</p>
                     </div>
                 </div>
             </div>
