8ddb668d485d9ac3b2e735a417609f6addb9bf3c
Michal Bonowicz <michus1332@gmail.com>
2021-11-30 15:50:47 +0100

    add animation to vector2 in heropage

--- src/components/heroPage.js
+++ src/components/heroPage.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 
 import '../styles/heroPage.css'
 
 function HeroPage() {
+    const [vector2Move, setVector2Move] = useState(false)
+
+    const mouseEnter = (e) => {
+        setVector2Move(prevState => true)
+    }
+
+    const mouseLeave = (e) => {
+        setVector2Move(prevState => false)
+    }
+
     return (
         <div className="containerHero">
             <section className="containerHero__wrapper">
                 <div className="containerHero__heroImage">
                     <img className="containerHero__heroImage-vector1" src={require('../svg/vector1.svg').default} alt="vector1" />
                     <img className="containerHero__heroImage-img" src={require('../images/pexels-anna-shvets-4315839.png').default} alt="anna" />
-                    <img className="containerHero__heroImage-vector2" src={require('../svg/vector2.svg').default} alt="vector2" />
+                    <img onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} className={vector2Move ? "containerHero__heroImage-vector2 -move" : "containerHero__heroImage-vector2"} src={require('../svg/vector2.svg').default} alt="vector2" />
                 </div>
 
                 <section className="container__heroText">
@@ -20,7 +30,16 @@ function HeroPage() {
             </section>
             <div className="containerHero__textRoll">
                 <div className="containerHero__textRoll__content">
-                    <p>the hottest hits</p>
+                    <ul className="containerHero__textRoll-items">
+                        <li className="containerHero__textRoll-item">the hottest hits</li>
+                        <li className="containerHero__textRoll-item">the hottest hits</li>
+                        <li className="containerHero__textRoll-item">the hottest hits</li>
+                        <li className="containerHero__textRoll-item">the hottest hits</li>
+                        <li className="containerHero__textRoll-item">the hottest hits</li>
+                        <li className="containerHero__textRoll-item">the hottest hits</li>
+                        <li className="containerHero__textRoll-item">the hottest hits</li>
+                        <li className="containerHero__textRoll-item">the hottest hits</li>
+                    </ul>
                 </div>
             </div>
         </div >
