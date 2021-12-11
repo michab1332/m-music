8210c254cc3708ce25d34e8f017df19039408d0e
Michal Bonowicz <michus1332@gmail.com>
2021-12-11 22:36:55 +0100

    add styles to toptrackitem

--- src/components/topTracksSectionElements/topTrackItem.js
+++ src/components/topTracksSectionElements/topTrackItem.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function TopTrackItem() {
+function TopTrackItem({ black }) {
     return (
-        <div className="containerTopTrackItem">
+        <div className="containerTopTrackItem" style={black ? { color: "#fff" } : { color: "#000" }}>
             <div className="containerTopTrackItem__content">
-                <img src="https://t2.genius.com/unsafe/409x409/https%3A%2F%2Fimages.genius.com%2Fa1dad51f2cc2782a8b7108a931420c3e.1000x1000x1.jpg" alt="big mommy"></img>
+                <img className="containerTopTrackItem__content-img" src="https://t2.genius.com/unsafe/409x409/https%3A%2F%2Fimages.genius.com%2Fa1dad51f2cc2782a8b7108a931420c3e.1000x1000x1.jpg" alt="big mommy"></img>
+                <div className="containerTopTrackItem__content__textWrapper">
+                    <p className="containerTopTrackItem__content__textWrapper-name">Oliwka Brazylia</p>
+                    <p className="containerTopTrackItem__content__textWrapper-songName">Duza mama</p>
+                </div>
             </div>
+            <p className="containerTopTrackItem__number">1</p>
         </div>
     )
 }
--- src/components/topTracksSectionElements/topTrackSection.js
+++ src/components/topTracksSectionElements/topTrackSection.js
@@ -8,9 +8,11 @@ function TopTrackSection({ name, color, black }) {
     return (
         <div className="containerTopTrack" style={black ? { backgroundColor: "#000" } : { backgroundColor: "#fff" }}>
             <h1 className="containerTopTrack__titleText" style={black ? { color: "#fff" } : { color: "#000" }}>Top Tracks in the <span style={{ color: color }}>{name}</span></h1>
-            <TopTrackItem />
-            <TopTrackItem />
-            <TopTrackItem />
+            <TopTrackItem black={black} />
+            <TopTrackItem black={black} />
+            <TopTrackItem black={black} />
+            <TopTrackItem black={black} />
+            <TopTrackItem black={black} />
         </div>
     )
 }
